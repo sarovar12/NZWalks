749f7e28e4e06db569c840d68d8fa5460d623e0d
sarovar12 <85166131+sarovar12@users.noreply.github.com>
2023-08-01 18:56:09 +0545

    Add project files.

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:portnumber/api/regions 
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        //GET ALL REGIONS
+        //GET: https://localhost:portnumber/api/regions 
+
+        [HttpGet]    
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name="Auckland Region",
+                    Code="AKL",
+                    RegionImageURL="https://images.pexels.com/photos/17319423/pexels-photo-17319423/free-photo-of-ornamented-walls-around-glass-ceiling-in-dome.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                },
+                new Region {
+                     Id = Guid.NewGuid(),
+                    Name="Wellington Region",
+                    Code="WLG",
+                    RegionImageURL="https://images.pexels.com/photos/17319423/pexels-photo-17319423/free-photo-of-ornamented-walls-around-glass-ceiling-in-dome.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+                },
+            };
+
+            return Ok(regions);
+        }
+    }
+}
---
+++ NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions dbContextOptions) :base(dbContextOptions)
+        {
+               
+        }
+
+
+        //Create Tables/relation from /Models/Domain in database
+        public DbSet<Difficulty> Difficulties { get; set; } 
+        public DbSet<Region> Regions { get; set; } 
+        public DbSet<Walk> Walks { get; set; } 
+
+    }
+}
